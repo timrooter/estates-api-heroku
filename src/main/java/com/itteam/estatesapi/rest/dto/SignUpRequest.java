38f8a17df09b95acabef7058d548cbf9e595b8ab
Timur <anvarov360@gmail.com>
2024-05-20 23:16:21 +0500

    first commit

---
+++ src/main/java/com/itteam/estatesapi/rest/dto/SignUpRequest.java
@@ -0,0 +1,27 @@
+package com.itteam.estatesapi.rest.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+public class SignUpRequest {
+
+    @Schema(example = "user3")
+    @NotBlank
+    private String username;
+
+    @Schema(example = "user3")
+    @NotBlank
+    private String password;
+
+    @Schema(example = "User3")
+    @NotBlank
+    private String name;
+
+    @Schema(example = "user3@mycompany.com")
+    @Email
+    private String email;
+}
